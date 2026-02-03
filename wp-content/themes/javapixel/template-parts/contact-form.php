5314e96d656315ac69e6a572122cbe548bec4de8
ksondresen <ksondresen@gmail.com>
2026-02-03 11:44:55 +0100

    updates

--- wp-content/themes/javapixel/template-parts/contact-form.php
+++ wp-content/themes/javapixel/template-parts/contact-form.php
@@ -3,19 +3,25 @@
             <img src="wp-content\themes\javapixel\assets\images\230919_laptop_and_plant_photo.png" style="width:100%">
         </div>        
         <form method="post" action="http://localhost/javapixel/" class="contact-form-radio-button" style="display:flex; flex-direction: column;gap:10px; padding-left: 20px">
-            <fieldset style="all: unset; display:flex; flex-direction: column;gap:10px; ">
-            <legend style="
+            <fieldset style="
+                all: unset; 
+                display:flex; 
+                flex-direction: column;
+                gap:10px; 
+                width: 350px;
+                ">
+            <p style="
                 margin-bottom:13px;
-                width: 300px;
-
                 ">                
+                <!-- orginal utkast
                 Vår kundemøter er fullstendig fri for slitsomme innsalg og kjøpspress. 
                 Vi er ikke selgere, og er derfor notorisk dårlige på å fikse deals. 
                 Men vi elsker å lage gode nettsider og synes alltid det er gøy og bli kjent med nye prosjekter. 
                 Vi kan love deg at våre møter er lave skuldre og gode løsninger, om du skulle trenge oss. 
+                -->
 
-                <br><br>Ønsker du et gratis og uforpliktende møte med oss?
-            </legend>
+                Ønsker du et gratis og uforpliktende møte med oss?
+        </p>
             
             <label 
                 style="
@@ -28,6 +34,7 @@
                 <input type="radio" name="radio_option" id="option1" value="Option 1" onclick="toggleTextField()" checked >
                 Ja!
             </label>
+            
             <!--
                 <label 
                     style="
@@ -77,6 +84,10 @@
                 <input type="email" name="email_field" placeholder="Din epost" style="all: unset;background-color:#f3f3f3ff; padding: 10px; border-radius: 5px">
                 <input type="submit" name="form_submit_button" value="Send" style="all: unset;background-color:#cdf4ffff; padding: 10px; border-radius: 5px">
             </div>
+            <p>
+                Våre kundemøter er helt fri for kjøpspress og salgstaktikker. Vi er ikke selgere -                 
+                bare et lite team som er glad i gode prosjekter, gode relasjoner og løsninger som fungerer.
+            </p>
             </fieldset>
 
 
